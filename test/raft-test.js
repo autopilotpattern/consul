8746e4685e6e8b9efd30ed17198289b866277cc3
geek <wpreul@gmail.com>
2016-12-29 12:18:12 -0600

    Fix tests

--- test/raft-test.js
+++ test/raft-test.js
@@ -1,14 +1,14 @@
 #!/usr/local/bin/node
 var Docker = require('dockerode');
 var Consul = require('consul');
-var async = require('async');
+var Async = require('async');
 
 // dockerode will automatically pick up your DOCKER_HOST, DOCKER_CERT_PATH
 // but we need to set the version explicitly to support Triton
 // ref https://github.com/apocas/dockerode/issues/154
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 var docker = new Docker();
-docker.version = 'v1.20';
+docker.version = 'v1.24';
 
 listConsul(function (err, consulNodes) {
     if (err) {
@@ -41,7 +41,7 @@ function run3NodeTests(consulNodes) {
             return;
         }
         console.log('raft is healthy: ', result);
-        async.series([
+        Async.series([
             function (cb) { test3_1(consulNodes, cb); },
             function (cb) { test3_2(consulNodes, cb); },
             function (cb) { test3_3(consulNodes, cb); }
@@ -70,7 +70,7 @@ function run5NodeTests(consulNodes) {
             return;
         }
         console.log('raft is healthy: ', result);
-        async.series([
+        Async.series([
             function (cb) { test5_1(consulNodes, cb); },
             function (cb) { test5_2(consulNodes, cb); }
         ], function (errResult, testResults) {
@@ -97,11 +97,11 @@ function test3_1(consulNodes, callback) {
         consul2 = consulNodes[1],
         consul3 = consulNodes[2];
 
-    async.series([
-        function (cb) { stop(consul3, cb); },
-        function (cb) { waitForElection([consul1, consul2], cb); },
-        function (cb) { start(consul3, cb); },
-        function (cb) { waitForElection(consulNodes, cb); }
+    Async.series([
+        function (next) { stop(consul3, next); },
+        function (next) { waitForElection([consul1, consul2], next); },
+        function (next) { start(consul3, next); },
+        function (next) { waitForElection(consulNodes, next); }
     ], function (err, results) {
         callback(err, results[results.length - 1]);
     });
@@ -114,7 +114,7 @@ function test3_2(consulNodes, callback) {
         consul2 = consulNodes[1],
         consul3 = consulNodes[2];
 
-    async.series([
+    Async.series([
         function (cb) { stop(consul1, cb); },
         function (cb) { waitForElection([consul2, consul3], cb); },
         function (cb) { start(consul1, cb); },
@@ -132,7 +132,7 @@ function test3_3(consulNodes, callback) {
         consul2 = consulNodes[1],
         consul3 = consulNodes[2];
 
-    async.series([
+    Async.series([
         function (cb) { stop(consul1, cb); },
         function (cb) { stop(consul3, cb); },
         function (cb) { start(consul3, cb); },
@@ -153,7 +153,7 @@ function test5_1(consulNodes, callback) {
         consul4 = consulNodes[3],
         consul5 = consulNodes[4];
 
-    async.series([
+    Async.series([
         function (cb) { stop(consul1, cb); },
         function (cb) { stop(consul2, cb); },
         function (cb) { testWrites([consul3, consul4, consul5],
@@ -179,7 +179,7 @@ function test5_2(consulNodes, callback) {
         consul4 = consulNodes[3],
         consul5 = consulNodes[4];
 
-    async.series([
+    Async.series([
         function (cb) { createNetsplit([consul1, consul2],
                                        [consul3, consul4, consul5], cb); },
         function (cb) { testWrites([consul1, consul2],
@@ -199,7 +199,7 @@ function test5_2(consulNodes, callback) {
 
 // Queries Consul to determine the status of the raft. Compares the status
 // against a list of containers and verifies that the leader is among those
-// nodes. If failing, will retry twice with some backoff and then return
+// nodes. If failing, will retry 10 times with some backoff and then return
 // error to the callback if the raft still has not healed.
 // @param    {containers} array of container objects from our Consul nodes
 //           array that should be members of the raft.
@@ -208,29 +208,32 @@ function waitForElection(containers, callback) {
 
     var expected = [];
     containers.forEach(function (container) {
-        expected.push(container.Ip+':8300');
+        expected.push(container.Ip + ':8300');
     });
     expected.sort();
     console.log('expected peers:', expected.toString());
 
     var isMatch = false;
     var count = 0;
-    var maxCount = 3;
+    var maxCount = 10;
 
-    async.doUntil(
-        function (cb) {
-            getLeader(containers[0], function (err, leader) {
-                if (err || !leader) {
-                    cb(err);
-                    return;
-                }
-                isMatch = (expected.indexOf(leader) != -1);
-                cb(null);
-            });
+    Async.doUntil(
+        function (next) {
+            setTimeout(function () {
+                getLeader(containers[0], function (err, leader) {
+                    if (err && err.statusCode === 409) {            // The container is restarting, wait
+                        return next();
+                    }
+                    if (err || !leader) {
+                        return next(err);
+                    }
+                    isMatch = (expected.indexOf(leader) !== -1);
+                    next();
+                });
+            }, 1000);
         },
         function () {
-            count++;
-            return (isMatch || count >= maxCount);
+            return (isMatch || (++count >= maxCount));
         },
         function (err) {
             if (err) {
@@ -277,34 +280,20 @@ function listConsul(callback) {
     var consul = [];
     docker.listContainers(
         {all: false,
-         filters: ['{"label":["com.docker.compose.service=consul"]}']},
+         filters: { label: ['com.docker.compose.service=consul'] }},
         function (err, containers) {
             if (err) {
                 callback(err, null);
                 return;
             }
-            async.each(containers, function (container, cb) {
-                var cmd = ['ip', 'addr', 'show', 'eth0'];
-                runExec(container, cmd, function (e, ip) {
-                    if (e) {
-                        cb(e);
-                        return;
-                    }
-                    container['Ip'] = matchIp(ip);
-                    cb(null);
-                });
-            }, function (inspectErr) {
-                if (inspectErr) {
-                    callback(inspectErr, null);
-                }
-                containers.forEach(function (container) {
-                    container['Name'] = container.Names[0].replace('/', '');
-                    consul.push(container);
-                });
-                consul.sort(byName);
-                callback(null, consul);
-                return;
+
+            containers.forEach(function (container) {
+                container.Name = container.Names[0].replace('/', '');
+                container.Ip = container.NetworkSettings.Networks.consul_default.IPAddress;
+                consul.push(container);
             });
+            consul.sort(byName);
+            callback(null, consul);
         });
 }
 
@@ -316,13 +305,37 @@ function byName(a, b) {
 // @callback {fn} function(err, result)
 function stop(container, fn) {
     console.log('stopping', container.Name);
-    docker.getContainer(container.Id).stop(fn);
+    var runningContainer = docker.getContainer(container.Id);
+    runningContainer.stop(function (err, result) { });
+
+    runningContainer.wait(fn);
 }
 
 // @callback {fn} function(err, result)
-function start(container, fn) {
+function start(container, callback) {
     console.log('starting', container.Name);
-    docker.getContainer(container.Id).start(fn);
+    var containerInstance = docker.getContainer(container.Id);
+    containerInstance.start((function (err) {
+        if (err) {
+          return callback(err);
+        }
+
+        var checkStarted = function () {
+            containerInstance.inspect(function (err, result) {
+                if (err) {
+                  return callback(err);
+                }
+
+                if (result.State.Status === 'running') {
+                  return callback(null, result);
+                }
+
+                setTimeout(checkStarted, 1000);
+            });
+        };
+
+        checkStarted();
+    }));
 }
 
 // @callback {fn} function(err, leader)
@@ -330,10 +343,16 @@ function getLeader(container, fn) {
     runExec(container,
             ['curl', '127.0.0.1:8500/v1/status/leader'],
             function (err, leader) {
-                if (err || leader === null) {
+                if (err || !leader) {
                     return fn(err, null);
                 }
-                return fn(null, matchIpPort(leader)[0]);
+
+                var leaderAddress = matchIpPort(leader);
+                if (!leaderAddress) {
+                  return fn();
+                }
+
+                return fn(null, leaderAddress[0]);
             });
 }
 
@@ -343,6 +362,7 @@ function getPeers(container, fn) {
     runExec(container,
             ['curl', '-s', '127.0.0.1:8500/v1/status/peers'],
             function (err, peers) {
+              console.log('PEERS: ' + peers)
                 if (err || peers === null) {
                     return fn(err, null);
                 }
@@ -376,14 +396,17 @@ function runExec(container, command, callback) {
 
     docker.getContainer(container.Id).exec(options, function (execErr, exec) {
         if (execErr) {
-            callback(execErr, null);
+            return callback(execErr, null);
         }
-        exec.start({Tty: true, Detach: true}, function (err, stream) {
+
+        exec.start({hijack: true, stdin: true}, function (err, stream) {
             if (err) {
-                callback(err, null);
+                return callback(err, null);
             }
+
+            stream.end('\n');
+
             var body = '';
-            stream.setEncoding('utf8');
             stream.once('error', function (error) {
                 callback(error, null);
             });
